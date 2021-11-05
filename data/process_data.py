9061806d8497952a639a35db8155414238a52059
Muhammad <mdeevan@yahoo.com>
2021-11-04 20:19:46 -0400

    added the DOCString to the process_data.py file

--- data/process_data.py
+++ data/process_data.py
@@ -4,14 +4,46 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    """load the csv formatted messags and categories files, join on common id, and return as a dataframe
+	
+    Parameters:
+    messages_filepath (str): path and name of the messages file containing data in a csv format
+    categories_filepath (str): path and name of the categories file containing data in a csv format
+      
+    Returns:
+    dataframe: dataframe object made by merging messages and categories
+    
+    
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
 
     # merge datasets
+    # Merge the messages and categories datasets using the common id
+	
     df = messages.merge(categories, how='left', on='id')
     return df
 
 def clean_data(df):
+    """Clean the data by extracting features, and naming the features
+    
+
+    
+    Use the first row of categories dataframe to create column names for the categories data.
+    Rename columns of categories with new column names.
+
+	
+    Parameters:
+    df (dataframe): dataframe formed by merging categories and messages on a common id
+      
+    Returns:
+    dataframe object: cleaned dataframe consisting of feature set (categories columns)
+    
+    
+    """
+    # Split the values in the categories column on the ; character so that each value becomes a separate column. You'll find this
+	# method very helpful! Make sure to set expand=True.
+	# https://pandas.pydata.org/pandas-docs/version/0.23/generated/pandas.Series.str.split.html
     # create a dataframe of the 36 individual category columns
     categories = df['categories'].str.split(";", expand=True)
 
@@ -29,7 +61,14 @@ def clean_data(df):
 
     for column in categories:
         # set each value to be the last character of the string
-        categories[column] = categories[column].str[-1:]
+        val = categories[column].str[-1:]
+
+		# specifically for the related column there are number of rows where the count is 2
+		# this is to conver a value greater than 1 to 1, and applies to all columns
+        if (val > 1) : 
+        	val = 1
+        
+        categories[column] = val  # categories[column].str[-1:]
 
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
@@ -47,7 +86,18 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """convert the dataframe into a sqllite DB and save in the filesystem
 
+	
+    Parameters:
+    df (dataframe): name of the dataframe
+    database_filename (str): path and name of the file to save on filesystem
+      
+    Returns:
+    dataframe object: cleaned dataframe consisting of feature set (categories columns)
+    
+    
+    """
     engine = create_engine(os.path.join('sqlite:///', database_filename))
     df.to_sql('DisasterResponse', engine, index=False)
 
